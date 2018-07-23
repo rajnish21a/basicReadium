51f30cbc1f2e4178b7181ca4b8dc8689cdd5a2ec
Amlan Roy <amlan.roy@learningmatekol.local>
2018-07-23 22:08:10 +0530

    initial_Commit

---
+++ dev/annotatorInit.js
@@ -0,0 +1,18 @@
+// setTimeout(function(){loadAnnotator()},6000);
+
+// var loadAnnotator = function(){
+//     $(document.body).annotator()
+// };
+
+// window.addEventListener("resize",function(){
+//     setTimeout(loadAnnotator,15000);
+// });
+
+// jQuery(function ($) {
+//     $(document.body).annotator();
+// });
+
+
+// $(document).ready(function(){
+//     $(document.body).annotator();
+// });
